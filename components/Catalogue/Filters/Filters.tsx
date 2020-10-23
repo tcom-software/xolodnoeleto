d4e48aaa78fdb0ae2f751a02c9bfc7bf4c78c0de
davidburnazyan <david.burnazyan.96@gmail.com>
2020-10-23 14:51:41 +0400

    start range

--- components/Catalogue/Filters/Filters.tsx
+++ components/Catalogue/Filters/Filters.tsx
@@ -1,8 +1,57 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { FiltersContainer } from "./styles";
+import { SvgIcon } from "@atoms";
 
 const Filters = ({ filters }) => {
-  return <FiltersContainer>asdfasdfasd</FiltersContainer>;
+  const [needOpen, setNeedOpen] = useState([]);
+
+  return (
+    <FiltersContainer>
+      {Object.values(filters).map((e: any, i) => {
+        if (e.type == "between") {
+          return (
+            <div className="between">
+              <div className="title">
+                {e.title}
+                <span></span>
+                <SvgIcon
+                  type={
+                    needOpen.indexOf(e.title) != -1 ? "arrowUp" : "arrowDown"
+                  }
+                  width={15}
+                  height={15}
+                  callback={() => {
+                    const index = needOpen.indexOf(e.title);
+                    if (index === -1) {
+                      setNeedOpen([e.title, ...needOpen]);
+                    } else {
+                      let newArray = needOpen;
+                      newArray.splice(index, 1);
+                      setNeedOpen([...newArray]);
+                    }
+                  }}
+                />
+              </div>
+              {needOpen.indexOf(e.title) != -1 ? (
+                <div className="show_hide_content">
+                  <div className="show_hide_content__from-to-and">
+                    <input type="number" placeholder="от" />
+                    <input type="number" placeholder="до" />
+                  </div>
+                  <div className="show_hide_content__between-new-style">
+                    kjhkjhkjhkjh
+                  </div>
+                </div>
+              ) : null}
+            </div>
+          );
+        } else if (e.type == "multipleCases") {
+          return null;
+          return <div className="multiple-cases">multipleCases</div>;
+        }
+      })}
+    </FiltersContainer>
+  );
 };
 
 export default Filters;
--- components/Catalogue/Filters/styles.tsx
+++ components/Catalogue/Filters/styles.tsx
@@ -1,5 +1,56 @@
 import styled from "styled-components";
+import theme from "styles/theme";
 
-const FiltersContainer = styled.div``;
+const FiltersContainer = styled.div`
+  background: #fff;
+
+  & > div {
+    padding: 15px;
+  }
+
+  .between {
+    display: flex;
+    flex-direction: column;
+    border-bottom: 1px solid ${theme.body.someBorder};
+
+    .title {
+      position: relative;
+      margin-bottom: 18px;
+      display: flex;
+      justify-content: space-between;
+
+      span {
+        position: absolute;
+        width: 2px;
+        height: 20px;
+        left: -15px;
+        top: 0px;
+        background: #898989;
+      }
+      svg {
+        position: absolute;
+        right: 0;
+        top: 0px;
+        cursor: pointer;
+      }
+    }
+
+    .show_hide_content {
+      &__from-to-and {
+        width: 100%;
+        display: flex;
+        justify-content: space-between;
+
+        input {
+          width: 45%;
+          padding: 5px 10px;
+          border: 1px solid ${theme.body.someBorder};
+        }
+      }
+      &__between-new-style {
+      }
+    }
+  }
+`;
 
 export { FiltersContainer };
