e0e9b5cdeaf89dca1260437ec01a0e0ca9c781f2
davidburnazyan <david.burnazyan.96@gmail.com>
2020-12-29 23:43:46 +0400

    change catalogue

--- pages/catalog/[catalogId].js
+++ pages/catalog/[catalogId].js
@@ -1,6 +1,6 @@
 import React from "react";
-import Catalog from "../../components/Catalog";
 import Head from "next/head";
+import Catalog from "../../components/Catalog";
 
 const Index = () => {
   return (
