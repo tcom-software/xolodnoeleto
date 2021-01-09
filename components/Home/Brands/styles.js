88b8a1faf20514092d35171e8e41a34b6195b00f
davidburnazyan <david.burnazyan.96@gmail.com>
2021-01-10 00:45:50 +0400

    brands card

--- components/Home/Brands/styles.js
+++ components/Home/Brands/styles.js
@@ -7,24 +7,30 @@ const BrandsContainer = styled.div`
     font-weight: 800;
   }
   .brands-list {
+    perspective: 1000px;
+    transform-style: preserve-3d;
+
     margin-top: 27px;
     display: grid;
     grid-gap: 20px;
     grid-template-columns: repeat(auto-fill, minmax(216px, 1fr));
-
-    .brand {
-      height: 100px;
-      display: flex;
-      justify-content: center;
-      align-items: center;
-      background: #ffffff;
-      border: 1px solid #d8d8d8;
-      box-sizing: border-box;
-      box-shadow: 0px 1px 2px rgba(0, 0, 0, 0.11);
-    }
   }
   @media (max-width: ${theme.mobileMedia.size}) {
   }
 `;
 
-export { BrandsContainer };
+const BrandItem = styled.div`
+  transition: transform 0.2s;
+  transform: rotateX(0deg);
+  will-change: transform;
+
+  height: 100px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: #ffffff;
+  border: 1px solid #d8d8d8;
+  box-sizing: border-box;
+  box-shadow: 0px 1px 2px rgba(0, 0, 0, 0.11);
+`;
+export { BrandsContainer, BrandItem };
