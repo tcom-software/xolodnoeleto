d23d6854a16dc7c5b99fb512a5a3e12a6146b070
davidburnazyan <david.burnazyan.96@gmail.com>
2021-01-14 14:29:10 +0400

    fix loading

--- components/Catalog/Products/Products.js
+++ components/Catalog/Products/Products.js
@@ -47,56 +47,53 @@ const Products = ({
     catalogId && getCatalogProducts(catalogId, { ...object });
   }, [router.query]);
 
+  if (productsLoading) {
+    return <Loading />;
+  }
   if (products.length === 0) {
     return <DataEmpty title={"Нет продуктов"} />;
   }
 
   return (
-    <>
-      {productsLoading ? (
-        <Loading />
+    <div className="catalog">
+      {isMobile ? (
+        <h1>ХИТЫ ПРОДАЖ</h1>
       ) : (
-        <div className="catalog">
-          {isMobile ? (
-            <h1>ХИТЫ ПРОДАЖ</h1>
-          ) : (
-            <div className="title-section">
-              <p>{total} Товаров</p>
-              <select
-                className="sort-by"
-                onChange={(event) => {
-                  updateSelectedOrderBy(event.target.value);
-                }}
-                value={selectedData.orderBy}
-              >
-                Цена: высокая-низкая
-                <option value="">Сортировать: по популярности</option>
-                <option value="ASC">Цена: низкая-высокая</option>
-                <option value="DESC">Цена: высокая-низкая</option>
-              </select>
-            </div>
-          )}
-
-          <Pagination
-            total={total}
-            page={router.query.page}
-            callback={updateSelectedDataPage}
-          />
-
-          <div className="products">
-            {Object.values(products).map((item, index) => {
-              return <ProductGridView key={index} item={item} />;
-            })}
-          </div>
-
-          <Pagination
-            total={total}
-            page={router.query.page}
-            callback={updateSelectedDataPage}
-          />
+        <div className="title-section">
+          <p>{total} Товаров</p>
+          <select
+            className="sort-by"
+            onChange={(event) => {
+              updateSelectedOrderBy(event.target.value);
+            }}
+            value={selectedData.orderBy}
+          >
+            Цена: высокая-низкая
+            <option value="">Сортировать: по популярности</option>
+            <option value="ASC">Цена: низкая-высокая</option>
+            <option value="DESC">Цена: высокая-низкая</option>
+          </select>
         </div>
       )}
-    </>
+
+      <Pagination
+        total={total}
+        page={router.query.page}
+        callback={updateSelectedDataPage}
+      />
+
+      <div className="products">
+        {Object.values(products).map((item, index) => {
+          return <ProductGridView key={index} item={item} />;
+        })}
+      </div>
+
+      <Pagination
+        total={total}
+        page={router.query.page}
+        callback={updateSelectedDataPage}
+      />
+    </div>
   );
 };
 export default Products;
