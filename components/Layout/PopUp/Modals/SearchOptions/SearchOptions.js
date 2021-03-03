d701fff8792c27261f268c574d42bbc183dcb2e4
looklikeupwork <looklikeupwork@gmail.com>
2021-03-03 04:53:20 -0800

    change back url

--- components/Layout/PopUp/Modals/SearchOptions/SearchOptions.js
+++ components/Layout/PopUp/Modals/SearchOptions/SearchOptions.js
@@ -10,49 +10,52 @@ const SearchOptions = ({
   selectedSearchCatalog,
   setSelectedSearchCatalog,
 }) => {
-  return (
-    <SearchOptionsContainer ref={modalRef}>
-      <div className="close-modal-search-option">
-        <Button
-          type="primary"
-          width="100px"
-          height="100%"
-          onClick={() => setSelectedSearchCatalog(null)}
-        >
-          Clear
-        </Button>
-        <SvgIcon
-          type="close"
-          width="15px"
-          height="15px"
-          callback={closeModal}
-        />
-      </div>
-      {lastLevelCatalogItems.map(({ ids, name }) => {
-        return (
+  if (lastLevelCatalogItems) {
+    return (
+      <SearchOptionsContainer ref={modalRef}>
+        <div className="close-modal-search-option">
           <Button
-            key={ids.join(",")}
             type="primary"
-            width="175px"
-            height="30px"
-            onClick={() => setSelectedSearchCatalog(ids.join(","))}
+            width="100px"
+            height="100%"
+            onClick={() => setSelectedSearchCatalog(null)}
           >
-            {selectedSearchCatalog == ids.join(",") ? (
-              <SvgIcon
-                type="star"
-                width="15px"
-                height="15px"
-                fill={theme.body.sunColor}
-                className="selected-catalog-with-star"
-              />
-            ) : null}
-
-            {name}
+            Clear
           </Button>
-        );
-      })}
-    </SearchOptionsContainer>
-  );
+          <SvgIcon
+            type="close"
+            width="15px"
+            height="15px"
+            callback={closeModal}
+          />
+        </div>
+        {lastLevelCatalogItems.map(({ ids, name }) => {
+          return (
+            <Button
+              key={ids.join(",")}
+              type="primary"
+              width="175px"
+              height="30px"
+              onClick={() => setSelectedSearchCatalog(ids.join(","))}
+            >
+              {selectedSearchCatalog == ids.join(",") ? (
+                <SvgIcon
+                  type="star"
+                  width="15px"
+                  height="15px"
+                  fill={theme.body.sunColor}
+                  className="selected-catalog-with-star"
+                />
+              ) : null}
+
+              {name}
+            </Button>
+          );
+        })}
+      </SearchOptionsContainer>
+    );
+  }
+  return null;
 };
 
 export default SearchOptions;
