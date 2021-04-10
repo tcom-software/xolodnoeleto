2d8f1746dad4ecda5b833b108e92800a6ced997c
davidburnazyan <david.burnazyan.96@gmail.com>
2021-04-10 15:19:19 +0400

    fix font link

--- styles/fonts.js
+++ styles/fonts.js
@@ -3,9 +3,8 @@ export const fontfaces = () => `
       font-family: 'Open-sans';
       font-weight: 300;
       font-style: normal;
-      src: url('/fonts/open-sans-v18-latin-regular.woff2') format('woff2'),
-        url('/fonts/open-sans-v18-latin-regular.woff') format('woff');
-      // font-display: swap;
-      font-display: block
+      src: url('/fonts/open-sans-v18-latin-regular.woff2') format('woff2');
+      font-display: swap;
+      
   }
 `;
