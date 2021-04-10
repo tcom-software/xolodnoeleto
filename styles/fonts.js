fbbec5cb8ee8e0a27eaafd7fe5d41b402434cdbd
davidburnazyan <david.burnazyan.96@gmail.com>
2021-04-10 15:13:54 +0400

    fix font link

--- styles/fonts.js
+++ styles/fonts.js
@@ -5,6 +5,7 @@ export const fontfaces = () => `
       font-style: normal;
       src: url('/fonts/open-sans-v18-latin-regular.woff2') format('woff2'),
         url('/fonts/open-sans-v18-latin-regular.woff') format('woff');
-      font-display: swap;
+      // font-display: swap;
+      font-display: block
   }
 `;
